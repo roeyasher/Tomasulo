0b601a815114a3a71172abf585f10f54741f7387
itaizlot <itay.zlot@gmail.com>
2015-03-24 01:08:33 +0200

    stom

---
+++ our_files/CDBFile.h
@@ -0,0 +1,47 @@
+#ifndef CDBFile
+#define CDBFile
+
+#include "common.h"
+
+typedef struct IntCDB IntCDB;
+struct IntCDB{
+
+	int OPCODE;
+	BOOL busy;
+	int operand1, operand2;
+	char LabelOfSupplier[LABEL_SIZE];
+	struct IntCDB *next;
+	int result;
+	BOOL valid;
+};
+
+typedef struct FPCDB FPCDB;
+struct FPCDB{
+
+	int OPCODE;
+	BOOL busy;
+	int operand1, operand2;
+	char LabelOfSupplier[LABEL_SIZE];
+	struct FPCDB *next;
+	int result;
+	BOOL valid;
+};
+
+typedef struct LoadCDB LoadCDB;
+struct LoadCDB{
+
+	int OPCODE;
+	BOOL busy;
+	int operand1, operand2;
+	char LabelOfSupplier[LABEL_SIZE];
+	struct LoadCDB *next;
+	int result;
+	BOOL valid;
+};
+void CDBControlInt(IntCDB *int_to_cdb);
+void CDBControlFP(FPCDB *fp_to_cdb);
+void CDBControlLoad(LoadCDB *load_to_cdb);
+#endif
+
+
+
