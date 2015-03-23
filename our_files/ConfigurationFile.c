faa64a87187d6fe8ec76984a8b48abc141128438
roeyasher <roey.asher@gmail.com>
2015-03-23 04:10:54 +0200

    sdf

--- our_files/ConfigurationFile.c
+++ our_files/ConfigurationFile.c
@@ -2,62 +2,72 @@
 #include "shared.h"
 
 void InitializeConfiguration(char *ConfigFile){
-	char buffer[BUFFER_SIZE];
+	char buff[BUFFER_SIZE];
 	int i=0,j=0;
 	FILE *file=NULL;
 	Configuration_Data *Config_Pointer=(Configuration_Data*)malloc(sizeof(Configuration_Data));
 	file = FileOpen(ConfigFile, "rt");
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->int_delay=atoi(buffer)+1;
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->add_delay=atoi(buffer)+1;
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->mul_delay=atoi(buffer)+1;
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->mem_delay=atoi(buffer);
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->rob_entries=atoi(buffer);
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->add_nr_reservation=atoi(buffer);
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->mul_nr_reservation=atoi(buffer);
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
-	Config_Pointer->int_nr_reservation=atoi(buffer);
-	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
-	}
 
-	Config_Pointer->mem_nr_load_buffers=atoi(buffer);
 	for (i=0;i<3;i++){
-			memset(buffer,0,BUFFER_SIZE);
-			fscanf(file,"%s",buffer);
+		memset(buff, 0, BUFFER_SIZE);
+			fscanf(file,"%s",buff);
 	}
-	Config_Pointer->mem_nr_store_buffers=atoi(buffer);//fix after debug
+	Config_Pointer->int_delay = atoi(buff) + 1;
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->add_delay=atoi(buff)+1;
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->mul_delay=atoi(buff)+1;
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->mem_delay=atoi(buff);
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->rob_entries=atoi(buff);
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->add_nr_reservation=atoi(buff);
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->mul_nr_reservation=atoi(buff);
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->int_nr_reservation=atoi(buff);
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->mem_nr_load_buffers=atoi(buff);
+
+	for (i=0;i<3;i++){
+			memset(buff,0,BUFFER_SIZE);
+			fscanf(file,"%s",buff);
+	}
+	Config_Pointer->mem_nr_store_buffers=atoi(buff);//fix after debug
+
 	Configuration=Config_Pointer;
 	fclose(file);
 }
